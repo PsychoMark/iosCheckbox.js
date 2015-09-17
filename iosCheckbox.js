f1e3cd01845ab9217d00a4b36d8084789a989763
PsychoMark <PsychoMark@users.noreply.github.com>
2015-09-17 13:44:43 +0200

    Added support for multiple styles in one page Added custom CSS class option Added check if checkbox is already an iosCheckbox (multiple calls would result in multiple checkboxes for each input element) Two-way synchronization of check state, for form label and JS checkbox manipulation support

--- iosCheckbox.js
+++ iosCheckbox.js
@@ -1,44 +1,78 @@
 /**
  * iosCheckbox.js
- * Version: 1.0.0
- * Author: Ron Masas
+ * Version: 2.0.0
+ *
+ * Fork of iosCheckbox.js by Ron Masas
+ * https://github.com/masasron/iosCheckbox.js
+ *
+ * Fork by: Mark van Renswoude
  */
 
-(function( $ ){
-    $.fn.extend({
-        iosCheckbox: function ( ) {
-			$(this).each(function (){
-			       /**
-				* Original checkbox element
-				*/
-				var org_checkbox = $(this);
-			       /**
-				* iOS checkbox div
-				*/
-				var ios_checkbox = jQuery("<div>",{class: 'ios-ui-select'}).append(jQuery("<div>",{class: 'inner'}));
-				
-				// If the original checkbox is checked, add checked class to the ios checkbox.
-				if (org_checkbox.is(":checked")){
-					ios_checkbox.addClass("checked");
-				}
-				// Hide the original checkbox and print the new one.
-				org_checkbox.hide().after(ios_checkbox);
-				// Add click event listener to the ios checkbox
-				ios_checkbox.click(function (){
-					// Toggel the check state
-					ios_checkbox.toggleClass("checked");
-					// Check if the ios checkbox is checked
-					if (ios_checkbox.hasClass("checked")){
-						// Update state
-						org_checkbox.prop('checked', true);
-					}else{
-						// Update state
-						org_checkbox.prop('checked', false);
-					}
+(function($)
+{
+    $.fn.extend(
+      {
+        iosCheckbox: function(options)
+        {
+          function getParameter(element, name, defaultValue)
+          {
+            var data = $(element).data(name);
+            if (data)
+              return data;
 
-                    org_checkbox.change();
-				});
-			});
+            if (typeof options !== 'undefined' && options[name])
+              return options[name];
+
+            return defaultValue;
+          }
+
+
+          function toggle(org_checkbox, ios_checkbox)
+          {
+            ios_checkbox.toggleClass("checked");
+
+            org_checkbox.prop('checked', ios_checkbox.hasClass("checked"));
+            org_checkbox.change();
+          }
+
+
+          $(this).each(function()
+          {
+            var org_checkbox = $(this);
+
+            // Determine if we've been here before...
+            // For now modifications aren't supported, but this would be the place to add it.
+            if (org_checkbox.data('iosCheckboxified'))
+              return;
+
+            var style = getParameter(org_checkbox, 'style', 'slide');
+            var extraClass = getParameter(org_checkbox, 'class', null);
+
+            org_checkbox.data('iosCheckboxified', true);
+            var ios_checkbox = $('<div>', { class: 'ios-ui-' + style }).append($('<div>', { class: 'inner' }));
+
+            if (org_checkbox.is(':checked'))
+              ios_checkbox.addClass('checked');
+
+            if (extraClass)
+              ios_checkbox.addClass(extraClass);
+
+            // Capture changes to the original checkbox to support
+            // <label for="[org_checkbox id]"> tags
+            org_checkbox.change(function()
+            {
+              if (org_checkbox.is(':checked') != ios_checkbox.hasClass('checked'))
+                toggle(org_checkbox, ios_checkbox);
+            })
+
+            // Hide the original checkbox
+            org_checkbox.hide().after(ios_checkbox);
+
+            ios_checkbox.click(function()
+            {
+              toggle(org_checkbox, ios_checkbox);
+				    });
+			    });
         }
     });
 })(jQuery);
